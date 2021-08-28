c38dbe4cc94ba13a20f89bff53851c5534d22699
LachlanCourt <lachlan.m.court@gmail.com>
2021-08-28 17:09:53 +1000

    Add unique identifier

--- src/P1/WAR.java
+++ src/P1/WAR.java
@@ -4,20 +4,22 @@ public class WAR implements Runnable
 {
     private int track = 0;
     private boolean status;
+    String name;
     Intersection intersection;
 
     public WAR() {}
 
-    public WAR(int track_, boolean status_, Intersection intersection_)
+    public WAR(int track_, boolean status_, int identifier, Intersection intersection_)
     {
         track = track_;
         status = status_;
+        name = "WAR-" + String.valueOf(identifier);
         intersection = intersection_;
     }
 
     @Override public void run()
     {
-        // System.out.println("track: " + track + " status: " + status);
+        System.out.println(name + " Coming online:\nTrack: " + track + " status: " + status);
         while (intersection.getRunning())
         {
             intersection.reportPass(track);
