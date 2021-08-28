d42199ea956fa2c455d63c4789ea114b0f728293
LachlanCourt <lachlan.m.court@gmail.com>
2021-08-29 08:58:59 +1000

    We might be close

--- src/P2/P2.java
+++ src/P2/P2.java
@@ -29,15 +29,20 @@ public class P2
         }
 
         Printer printer = new Printer(jobs);
-
+        ArrayList<PrintHead> printHeads = new ArrayList();
         for (int i = 0; i < 3; i++)
         {
-            threads.add(new Thread(new PrintHead(printer)));
+            PrintHead temp = new PrintHead(printer);
+            threads.add(new Thread(temp));
+            printHeads.add(temp);
         }
         for (Thread thread : threads)
         {
             thread.start();
         }
+        // Start the printer to track global time in the main thread
+        printer.setPrintHeads(printHeads);
+        printer.start();
     }
 
     public ArrayList<Job> generateFromFile(String filename) throws Exception
--- src/P2/PrintHead.java
+++ src/P2/PrintHead.java
@@ -2,7 +2,45 @@ package P2;
 
 public class PrintHead implements Runnable
 {
-    public PrintHead(Printer printer) {}
+    private Printer printer;
+    private boolean printing;
+    private int startTime;
 
-    @Override public void run() {}
+    public PrintHead(Printer printer_)
+    {
+        printer = printer_;
+        printing = false;
+        startTime = 0;
+    }
+
+    public boolean printing()
+    {
+        return printing;
+    }
+
+    @Override public void run()
+    {
+        Job job = null;
+        while (!printer.isFinished())
+        {
+            synchronized (this) {
+                if (printer.validJobNext()) {
+                    job = printer.getJob();
+                }
+
+                if (job == null) {
+                    break;
+                }
+                printing = true;
+            }
+
+            if (printing) {
+                while (printer.getTime() < startTime + job.getSize()) {
+                    // vibe
+                }
+                System.out.println(printer.getTime() + " job: " + job.getId());
+                printing = false;
+            }
+        }
+    }
 }
